3cddb51b08c3ea2871f8ce1d3cc1f790b181bb9a
dhagesakshi <119070565+dhagesakshi@users.noreply.github.com>
2022-12-12 23:14:14 +0530

    Add files via upload

---
+++ Class092-Join and Split_Method-CipherSchool.py
@@ -0,0 +1,12 @@
+# split method
+user_info='arpit 24'.split()
+print(user_info)
+
+name,age='Sakshi 18'.split(",")
+print(name)
+print(age)
+
+
+#joint method
+user_info=['Sakshi','18']
+print(','.join(user_info))
---
+++ Class120-Update-CipherSchool.py
@@ -0,0 +1,7 @@
+user_info= {
+    'name': 'Sakshi',
+    'age' : '18',
+}
+more_info={'name' : 'Sakshi' , 'state' : 'Punjab' , 'Hobbies' : ['coding' , 'reading']}
+user_info.update(more_info)
+print(user_info)
